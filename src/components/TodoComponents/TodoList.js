62e1bc1e914183ffbe6b36b6c33c4b9d9ddd2c16
Ruth Poliakon <r.poliakon@gmail.com>
2019-10-01 20:05:38 -0600

    TodoList consumes data array and renders Todo component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,26 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+import data from "../../data";
+
+class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: data
+    };
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.todo.map(item => (
+          <Todo key={item.id} item={item} />
+        ))}
+      </>
+    );
+  }
+}
+
+export default TodoList;
